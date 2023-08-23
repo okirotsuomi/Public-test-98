9da13c812bbca808aa5b9367fde241395cc3c55a
okirotsuomi <okiro.abo@gmail.com>
2023-08-23 20:12:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+money = int(input('Введите сумму котроую хотите положить под проценты: '))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+deposit.append(int(per_cent['ТКБ']*money/100))
+deposit.append(int(per_cent['СКБ']*money/100))
+deposit.append(int(per_cent['ВТБ']*money/100))
+deposit.append(int(per_cent['СБЕР']*money/100))
+print('Максимальная сумма, которую вы можете заработать —', max(deposit))
