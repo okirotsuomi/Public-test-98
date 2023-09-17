aa42cf3baa78e9c9d8f1f4e853a3bd51200cd8c6
okirotsuomi <140109434+okirotsuomi@users.noreply.github.com>
2023-09-17 18:58:58 +0300

    Add files via upload
    Задание 21.7.1

---
+++ Задание 21.7.1 (object).py
@@ -0,0 +1,15 @@
+from pets import Cat
+
+pet1 = Cat('Барон', 'мальчик', 2)
+
+print('Имя первого кота:', pet1.getName())
+print('Пол первого кота:', pet1.getGender())
+print('Возраст первого кота:', pet1.getAge())
+
+print()
+
+pet2 = Cat('Сэм', 'мальчик', 2)
+
+print('Имя второго кота:', pet2.getName())
+print('Пол второго кота:', pet2.getGender())
+print('Возраст второго кота:', pet2.getAge())
